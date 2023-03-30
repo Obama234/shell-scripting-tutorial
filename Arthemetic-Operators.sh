3020f2faad7b53cffe4824f895347f85c4f34f88
sai kumar <117585094+Obama234@users.noreply.github.com>
2023-03-30 17:56:28 +0530

    little change

--- Arthemetic-Operators.sh
+++ Arthemetic-Operators.sh
@@ -6,10 +6,8 @@
 #Author: Ankam Ravi Kumar
 # START #
 
-echo -e "Please enter some value: \c"
-read -r a
-echo -e "Please enter another value: \c"
-read -r b
+read -p " please enter value of a:" a
+read -p "please enter value of b :" b 
 
 echo "a+b value is $(($a+$b))"    #it will add both the values
 echo "a-b value is $(($a-$b))"    #it will subtract b form a 
